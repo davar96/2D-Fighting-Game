5acdf5f3fbda94068092caf1c3a1caa4e4493ad8
David Arustamyan <david.arustamyan.96@gmail.com>
2022-05-17 23:44:06 +0200

    worked on attacking and health bars

--- script.js
+++ script.js
@@ -11,20 +11,45 @@ context.fillRect(0, 0, canvas.width, canvas.height);
 const gravity = 0.7;
 
 class Sprite {
-    constructor({ position, velocity }) {
+    constructor({ position, velocity, color = 'red', offset }) {
         this.position = position;
         this.velocity = velocity;
+        this.width = 50;
         this.height = 150;
         this.lastKey;
+        this.attackBox = {
+            position: {
+                x: this.position.x,
+                y: this.position.y,
+            },
+            offset,
+            width: 100,
+            height: 50,
+        };
+        this.color = color;
+        this.isAttacking;
+        this.health = 100;
     }
 
     draw() {
-        context.fillStyle = 'red';
-        context.fillRect(this.position.x, this.position.y, 50, this.height);
+        context.fillStyle = this.color;
+        context.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+        if (this.isAttacking) {
+            context.fillStyle = 'blue';
+            context.fillRect(
+                this.attackBox.position.x,
+                this.attackBox.position.y,
+                this.attackBox.width,
+                this.attackBox.height
+            );
+        }
     }
 
     update() {
         this.draw();
+        this.attackBox.position.x = this.position.x - this.attackBox.offset.x;
+        this.attackBox.position.y = this.position.y;
         this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
@@ -32,15 +57,26 @@ class Sprite {
             this.velocity.y = 0;
         } else this.velocity.y += gravity;
     }
+
+    attack() {
+        this.isAttacking = true;
+
+        setTimeout(() => {
+            this.isAttacking = false;
+        }, 100);
+    }
 }
 
 const player = new Sprite({
     position: { x: 0, y: 0 },
     velocity: { x: 0, y: 0 },
+    offset: { x: 0, y: 0 },
 });
 const enemy = new Sprite({
     position: { x: 400, y: 100 },
     velocity: { x: 0, y: 0 },
+    offset: { x: 50, y: 0 },
+    color: 'green',
 });
 
 player.draw();
@@ -67,6 +103,18 @@ const key = {
     },
 };
 
+function rectangularCollision({ rectangle1, rectangle2 }) {
+    return (
+        rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+        rectangle2.position.x &&
+        rectangle1.attackBox.position.x <=
+        rectangle2.position.x + rectangle2.width &&
+        rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+        rectangle2.position.y &&
+        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+    );
+}
+
 function animate() {
     window.requestAnimationFrame(animate);
     context.fillStyle = 'black';
@@ -88,6 +136,24 @@ function animate() {
     } else if (key.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
         enemy.velocity.x = 5;
     }
+
+    if (
+        rectangularCollision({ rectangle1: player, rectangle2: enemy }) &&
+        player.isAttacking
+    ) {
+        player.isAttacking = false;
+        enemy.health -= 10;
+        document.getElementById('enemyHealth').style.width = enemy.health + '%';
+    }
+
+    if (
+        rectangularCollision({ rectangle1: enemy, rectangle2: player }) &&
+        enemy.isAttacking
+    ) {
+        enemy.isAttacking = false;
+        player.health -= 10;
+        document.getElementById('playerHealth').style.width = player.health + '%';
+    }
 }
 
 animate();
@@ -105,6 +171,9 @@ window.addEventListener('keydown', event => {
         case 'z':
             player.velocity.y = -20;
             break;
+        case ' ':
+            player.attack();
+            break;
         case 'ArrowRight':
             key.ArrowRight.pressed = true;
             enemy.lastKey = 'ArrowRight';
@@ -116,6 +185,9 @@ window.addEventListener('keydown', event => {
         case 'ArrowUp':
             key.ArrowUp.pressed = true;
             enemy.velocity.y = -20;
+            break;
+        case 'Shift':
+            enemy.isAttacking = true;
             break;
     }
 });
